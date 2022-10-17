fde49696d3f85a476143906badcf698e3e5ae88b
Darragh ORiordan <darragh.oriordan@gmail.com>
2022-10-17 23:24:12 +1100

    add for images

---
+++ pages/api/roamog.tsx
@@ -0,0 +1,67 @@
+/* eslint-disable @next/next/no-img-element */
+import { ImageResponse } from "@vercel/og";
+import { NextRequest } from "next/server";
+import React from "react";
+export const config = {
+  runtime: "experimental-edge",
+};
+
+export default function handler(req: NextRequest): ImageResponse {
+  try {
+    const { searchParams } = new URL(req.url);
+
+    const hasTitle = searchParams.has("title");
+
+    // a string with 100 random characters
+    const longString = Array.from(
+      { length: 60 },
+      () => Math.random().toString(36)[2]
+    ).join(" ");
+
+    const title = hasTitle
+      ? searchParams.get("title")?.slice(0, 120)
+      : longString;
+
+    const response = new ImageResponse(
+      (
+        <div
+          tw={`flex flex-col bg-green-500 justify-between pt-15 px-10 pb-5`}
+          style={{ width: "1200px", height: "630px" }}
+        >
+          <div tw="flex text-7xl mb-10 grow mx-auto">
+            <p tw="p-0 m-0 text-center">{title}</p>
+          </div>
+          <div tw="flex items-end space-between shrink">
+            <div tw="flex flex-col mb-4">
+              <div tw="text-black font-mono text-6xl text-amber-50 mb-4">
+                @darraghorsdfsd
+              </div>
+              <div tw="text-black font-mono text-4xl">
+                https://www.darraghoriordan.com
+              </div>
+            </div>
+            <div tw="flex ml-60" style={{ height: "280px" }}>
+              <img
+                alt="profile image"
+                src={"/chisq.png"}
+                tw="rounded-full shadow-lg"
+                width={280}
+                height={280}
+              />
+            </div>
+          </div>
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 630,
+      }
+    );
+    return response;
+  } catch (e: any) {
+    console.log(`${e.message}`);
+    return new Response(`Failed to generate the image`, {
+      status: 500,
+    });
+  }
+}
